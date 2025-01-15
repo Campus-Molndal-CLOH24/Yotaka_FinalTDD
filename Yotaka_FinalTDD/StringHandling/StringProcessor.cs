c6bf251d3dd2213581cd021015ca1c13138c2d9a
Yotaka k. <88yotkha@gafe.molndal.se>
2025-01-15 23:19:46 +0100

    added string handler

---
+++ Yotaka_FinalTDD/StringHandling/StringProcessor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Yotaka_FinalTDD.StringHandling
+{
+    public class StringProcessor
+    {
+        public string ToLowerCase(string input)
+        {
+            return input.ToLower();
+        }
+        public string Sanitize(string input)
+        {
+            return Regex.Replace(input, "[^a-zA-Z0-9]", "");
+        }
+        public bool AreEqual(string input1, string input2)
+        {
+            string input1Sanitized = Sanitize(input1);
+            string input2Sanitized = Sanitize(input2);
+            return input1Sanitized.Equals(input2Sanitized, StringComparison.OrdinalIgnoreCase);
+        }
+        
+
+    }
+}
---
+++ Yotaka_FinalTDDTests/StringHandling/StringProcessorTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Yotaka_FinalTDD.StringHandling;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yotaka_FinalTDD.StringHandling.Tests
+{
+    [TestClass()]
+    public class StringProcessorTests
+    {
+        [TestMethod()]
+        public void ToLowerCase_Returncorrectresult()
+        {
+            // Arrange
+            StringProcessor stringProcessor = new StringProcessor();
+            // Act
+            string result = stringProcessor.ToLowerCase("HELLO");
+            // Assert
+            Assert.AreEqual("hello", result);
+        }
+        [TestMethod()]
+        public void Sanitize_delectallspecialtecken_returncorrectresult()
+        {
+            // Arrange
+            StringProcessor stringProcessor = new StringProcessor();
+            // Act
+            string result = stringProcessor.Sanitize("Hello!@#$");
+            // Assert
+            Assert.AreEqual("Hello", result);
+        }
+
+        [TestMethod()]
+        public void AreEqual_compare_returncorrectresult()
+        {
+            // Arrange
+            StringProcessor stringProcessor = new StringProcessor();
+            // Act
+            bool result = stringProcessor.AreEqual("Hello,World", "HELLO WORLD");
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+
+    }
+}
