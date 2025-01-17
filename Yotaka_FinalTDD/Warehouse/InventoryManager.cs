e42e12d00260bc5374e5f241594d9e6230303be8
Yotaka k. <88yotkha@gafe.molndal.se>
2025-01-17 23:57:51 +0100

    added invenyory manage test

---
+++ Yotaka_FinalTDD/Warehouse/InventoryManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yotaka_FinalTDD.Warehouse
+{
+    public class InventoryManager
+    {
+        public Dictionary<string, int> Items = new Dictionary<string, int>();
+        public void AddItem(string itemName, int quantity)
+        {
+            if (Items.ContainsKey(itemName))
+            {
+                Items[itemName] += quantity;
+            }
+            else
+            {
+                Items.Add(itemName, quantity);
+            }
+
+        }
+        public void RemoveItem(string itemName, int quantity)
+        {
+            throw new NotImplementedException();
+        }
+        public List<string> GetOutOfStockItems() 
+        {
+            return Items.Where(x => x.Value == 0).Select(x => x.Key).ToList();
+        }
+    }
+}
---
+++ Yotaka_FinalTDDTests/Warehouse/InventoryManagerTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Yotaka_FinalTDD.Warehouse;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yotaka_FinalTDD.Warehouse.Tests
+{
+    [TestClass()]
+    public class InventoryManagerTests
+    {
+        [TestMethod()]
+        public void AddItem_ShouldAddItem_returnCorrectQuatity()
+        {
+            // Arrange
+            InventoryManager inventoryManager = new InventoryManager();
+            string itemName = "apple";
+            int quantity = 10;
+            // Act
+            inventoryManager.AddItem(itemName, quantity);
+            // Assert
+            Assert.AreEqual(quantity, inventoryManager.Items[itemName], "Failed add item");
+        }
+
+        [TestMethod()]
+        public void RemoveItemTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetOutOfStockItemsTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
