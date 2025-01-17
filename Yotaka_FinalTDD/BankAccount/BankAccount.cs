befea47fc61a4a67e3be26ff5b004579565a58c6
Yotaka k. <88yotkha@gafe.molndal.se>
2025-01-17 19:17:59 +0100

    added testing to backaccount

---
+++ Yotaka_FinalTDD/BankAccount/BankAccount.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yotaka_FinalTDD.BankAccount
+{
+    public class BankAccount
+    {
+        public decimal _balance { get; set; }
+
+        public decimal Deposit(decimal amount)
+        {
+            if (amount > 0) 
+            {
+                _balance += amount;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("Can not deposite with negative value or zero");
+            }
+            return _balance;
+        }
+        public decimal Withdraw(decimal amount)
+        {
+            if (amount > 0 && amount <= _balance)
+            {
+                _balance -= amount;
+            }
+            else
+            {
+                throw new ArgumentException("Can not withdraw the value is  more than balance");
+            }
+            return _balance;
+        }
+    }
+}
---
+++ Yotaka_FinalTDDTests/BankAccount/BankAccountTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Yotaka_FinalTDD.BankAccount;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yotaka_FinalTDD.BankAccount.Tests
+{
+    [TestClass()]
+    public class BankAccountTests
+    {
+        private BankAccount _bankAccount;
+        [TestInitialize]
+        public void Setup()
+        {
+            // Initialize a fresh account before each test (aarange )
+            _bankAccount = new BankAccount();
+            _bankAccount.Deposit(100.0m); //initial deposit 100 
+        }
+        [TestMethod()]
+        [DataRow(500.0, 600.0)]
+        [DataRow(100.0, 200.0)]
+        public void Deposit_ShouldIncreasesBalance(double depositAmount, double ExpectedBalances)
+        {
+            //act
+            _bankAccount.Deposit((decimal)depositAmount);
+            
+            //assert
+            Assert.AreEqual((decimal)ExpectedBalances, _bankAccount._balance, "Failed deposit money");
+        }
+
+        [TestMethod]
+        [DataRow(-100.0)] //negative value
+        public void Deposit_NegativeValue_ShouldThrowException(double depositAmount)
+        {
+            //act and assert
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _bankAccount.Deposit((decimal)depositAmount), "Failed deposit money with exception");
+        }
+
+        [TestMethod()]
+        [DataRow(50.0, 50.0)]
+        [DataRow(100.0, 0.0)]
+        public void Withdraw_ShoulddecreasesBalances(double withdrawAmount, double ExpectedBalances)
+        {
+            //act
+            _bankAccount.Withdraw((decimal)withdrawAmount);
+            //assert
+            Assert.AreEqual((decimal)ExpectedBalances, _bankAccount._balance, "Failed withdraw money");
+
+        }
+        //withdraw more than balance
+        [TestMethod()]
+        [DataRow(200.0)]
+        public void Withdraw_MoreThanBalance_ShouldThrowException(double withdrawAmount)
+        {
+            //act and assert
+            Assert.ThrowsException<ArgumentException>(() => _bankAccount.Withdraw((decimal)withdrawAmount), "Failed withdraw money with exception");
+        }
+    }
+}
