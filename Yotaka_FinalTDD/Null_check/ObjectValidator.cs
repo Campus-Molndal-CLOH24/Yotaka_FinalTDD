adbdd8dec381006e8829dca503d10bc2175bbd31
Yotaka k. <88yotkha@gafe.molndal.se>
2025-01-18 11:49:02 +0100

    added isnull method

---
+++ Yotaka_FinalTDD/Null_check/ObjectValidator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yotaka_FinalTDD.Null_check
+{
+    public class ObjectValidator
+    {
+        public bool IsNull(object obj)
+        {
+            if (obj == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public List<string> GetNullProperties(object obj) 
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Yotaka_FinalTDDTests/Null_check/ObjectValidatorTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Yotaka_FinalTDD.Null_check;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yotaka_FinalTDD.Null_check.Tests
+{
+    [TestClass()]
+    public class ObjectValidatorTests
+    {
+        [TestMethod()]
+        public void IsNull_checkvalue_Retuencorrectlogisk()
+        {
+            // Arrange
+            ObjectValidator objectValidator = new ObjectValidator();
+            object obj = null;
+            object obj2 = null;
+            object obj3 = 50;
+            // Act
+            bool result = objectValidator.IsNull(obj);
+            bool result2 = objectValidator.IsNull(obj2);
+            bool result3 = objectValidator.IsNull(obj3);
+            // Assert
+            Assert.IsTrue(result, "Fail validation");
+            Assert.IsTrue(result2, "Fail validation");
+            Assert.IsFalse(result3, "Fail validation");
+        }
+
+        [TestMethod()]
+        public void GetNullPropertiesTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
