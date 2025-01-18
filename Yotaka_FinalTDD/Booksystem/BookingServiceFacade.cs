4cd5c35e682ea5dbedb027f3a32dedc3ae8a3e9b
Yotaka k. <88yotkha@gafe.molndal.se>
2025-01-18 12:47:14 +0100

    added methods to classes

---
+++ Yotaka_FinalTDD/Booksystem/BookingServiceFacade.cs
@@ -0,0 +1,18 @@
+﻿using NSubstitute.Routing.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yotaka_FinalTDD.Booksystem
+{
+    public class BookingServiceFacade
+    {
+        public async Task<bool> BookRoomAsync(string roomName, DateTime startDate, DateTime endDate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    
+}
---
+++ Yotaka_FinalTDD/Booksystem/BookingSystem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yotaka_FinalTDD.Booksystem
+{
+    public List<Booking> Bookings { get; set; } // List of bookings
+
+    public class BookingSystem
+    {
+        public async Task<bool> BookRoomAsync(string roomName, DateTime startDate, DateTime endDate)
+        {
+            throw new NotImplementedException();
+        }
+        public List<DateTime> GetAvailableTimeSlots()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
