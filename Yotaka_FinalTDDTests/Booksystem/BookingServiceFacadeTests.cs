2ac7e8f109afdaaa6e65b809ae74b646b07d556f
Yotaka k. <88yotkha@gafe.molndal.se>
2025-01-18 18:43:31 +0100

    starting to test facade class

---
+++ Yotaka_FinalTDDTests/Booksystem/BookingServiceFacadeTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Yotaka_FinalTDD.Booksystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yotaka_FinalTDD.Booksystem.Tests
+{
+    [TestClass()]
+    public class BookingServiceFacadeTests
+    {
+        [TestMethod()]
+        public void BookingServiceFacadeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BookSlotTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
