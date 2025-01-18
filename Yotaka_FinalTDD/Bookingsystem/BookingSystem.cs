115c9a49144cdb3241f0b410fa3e1eddf4b0ebe3
Yotaka k. <88yotkha@gafe.molndal.se>
2025-01-19 00:10:03 +0100

    delected some comment that don't need to

--- Yotaka_FinalTDD/Bookingsystem/BookingSystem.cs
+++ Yotaka_FinalTDD/Bookingsystem/BookingSystem.cs
@@ -10,7 +10,6 @@ namespace Yotaka_FinalTDD.Booksystem
 {
     public class BookingSystem : IRoomsystem
     {
-        // A list to keep track of all the bookings.
         public List<Booking> Bookings { get; private set; } = new();
         private readonly IRoomsystem _roomsystem;
         public BookingSystem(IRoomsystem roomsystem)
@@ -20,23 +19,22 @@ namespace Yotaka_FinalTDD.Booksystem
 
         public async Task<bool> BookTimeSlot(DateTime startDate, DateTime endDate)
         {
-            //check if the new booking is valid (start time is before end time)
+            
             if (startDate >= endDate)
             {
                 return false;
             }
             //check if the new time overlaps with any exiting bookings
+            // If the new time is not completely before or after an existing booking, it overlaps.
             foreach (var booking in Bookings)
             {
-                // If the new time is not completely before or after an existing booking, it overlaps.
+                
                 if (!(endDate <= booking.Starttime || startDate >= booking.Endtime))
                 {
                     return false;
                 }
             }
-            //if no overlap, add the new booking
             Bookings.Add(new Booking(startDate, endDate));
-            // Call the BookTimeSlot method on the _roomsystem mock changed from return true to return await _roomsystem.BookTimeSlot(startDate, Endtime) becuase mock data is not being used
             return await _roomsystem.BookTimeSlot(startDate, endDate); 
         }
 
