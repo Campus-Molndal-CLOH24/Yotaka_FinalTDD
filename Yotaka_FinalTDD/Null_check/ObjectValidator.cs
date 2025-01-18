45527f211f7859596f80a8d1cca46057298820a7
Yotaka k. <88yotkha@gafe.molndal.se>
2025-01-18 12:38:28 +0100

    added more personer class to handle with testing GetNullProperties class. and finnised testing null check class

--- Yotaka_FinalTDD/Null_check/ObjectValidator.cs
+++ Yotaka_FinalTDD/Null_check/ObjectValidator.cs
@@ -8,20 +8,26 @@ namespace Yotaka_FinalTDD.Null_check
 {
     public class ObjectValidator
     {
+        // Check if the object is null, return true if it is null and false if it is not null
         public bool IsNull(object obj)
         {
-            if (obj == null)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            return obj == null;
         }
         public List<string> GetNullProperties(object obj) 
         {
-            throw new NotImplementedException();
+            ArgumentNullException.ThrowIfNull(obj); // Throw an ArgumentNullException if the object is null
+
+            List<string> nullProperties = new List<string>();
+            var properties = obj.GetType().GetProperties();
+            foreach (var property in properties)
+            {
+                // Check if the property value is null
+                if (property.GetValue(obj) == null)
+                {
+                    nullProperties.Add(property.Name); // Add the property name to the list
+                }
+            }
+            return nullProperties; // Return the list of null property names
         }
     }
 }
--- Yotaka_FinalTDDTests/Null_check/ObjectValidatorTests.cs
+++ Yotaka_FinalTDDTests/Null_check/ObjectValidatorTests.cs
@@ -12,7 +12,7 @@ namespace Yotaka_FinalTDD.Null_check.Tests
     public class ObjectValidatorTests
     {
         [TestMethod()]
-        public void IsNull_checkvalue_Retuencorrectlogisk()
+        public void IsNull_ShouldReturnTrue_ForNullObject()
         {
             // Arrange
             ObjectValidator objectValidator = new ObjectValidator();
@@ -28,11 +28,59 @@ namespace Yotaka_FinalTDD.Null_check.Tests
             Assert.IsTrue(result2, "Fail validation");
             Assert.IsFalse(result3, "Fail validation");
         }
-
+        //return false
         [TestMethod()]
-        public void GetNullPropertiesTest()
+        public void IsNull_ShouldReturnFalse_ForNullObject()
         {
-            Assert.Fail();
+            // Arrange
+            ObjectValidator objectValidator = new ObjectValidator();
+            object obj = null;
+            object obj2 = "Great";
+            object obj3 = 10;
+            // Act
+            bool result = objectValidator.IsNull(obj);
+            bool result2 = objectValidator.IsNull(obj2);
+            bool result3 = objectValidator.IsNull(obj3);
+            // Assert
+            Assert.IsTrue(result, "Fail validation");
+            Assert.IsFalse(result2, "Fail validation");
+            Assert.IsFalse(result3, "Fail validation");
+        }
+        //returnerar rätt lista för en klass med vissa null-properties.
+        [TestMethod()]
+        public void GetNullProperties_ShouldReturnCorrectPropertyNames()
+        {
+            // Arrange
+            ObjectValidator objectValidator = new ObjectValidator();
+            var person = new Person { Name = "Gigi", Age = null , Adress = null};
+            
+            // Act
+            var result = objectValidator.GetNullProperties(person);
+            // Assert
+            CollectionAssert.AreEqual(new List<string> { "Age", "Adress" }, result); //return list of null properties
+        }
+        //handle with null throw exception
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void GetNullProperties_ShouldThrowException_ForNullObject()
+        {
+            // Arrange
+            ObjectValidator objectValidator = new ObjectValidator();
+            object obj = null;
+            // Act
+            objectValidator.GetNullProperties(obj);
+        }
+        //return an empty list for no null properties
+        [TestMethod()]
+        public void GetNullProperties_ShouldReturnEmptyList_ForNoNullProperties()
+        {
+            // Arrange
+            ObjectValidator objectValidator = new ObjectValidator();
+            var person = new Person { Name = "Gigi", Age = 20, Adress = "Stockholm" };
+            // Act
+            var result = objectValidator.GetNullProperties(person);
+            // Assert
+            CollectionAssert.AreEqual(new List<string>(), result); //return empty list
         }
     }
 }
